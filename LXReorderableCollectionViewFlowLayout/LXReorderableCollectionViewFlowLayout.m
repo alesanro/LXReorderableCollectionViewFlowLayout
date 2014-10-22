b5506277f914d2256792436abe02a05a8c5846c3
Alex Rouse <alex.rouse@raizlabs.com>
2014-10-22 14:32:18 -0400

    Fixing the snapshot scale issue on iPhone6

--- LXReorderableCollectionViewFlowLayout/LXReorderableCollectionViewFlowLayout.m
+++ LXReorderableCollectionViewFlowLayout/LXReorderableCollectionViewFlowLayout.m
@@ -50,15 +50,16 @@ static NSString * const kLXCollectionViewKeyPath = @"collectionView";
 @implementation UICollectionViewCell (LXReorderableCollectionViewFlowLayout)
 
 - (UIView *)LX_snapshotView {
-    if ([self respondsToSelector:@selector(snapshotViewAfterScreenUpdates:)]) {
-        return [self snapshotViewAfterScreenUpdates:YES];
-    } else {
-        UIGraphicsBeginImageContextWithOptions(self.bounds.size, self.isOpaque, 0.0f);
-        [self.layer renderInContext:UIGraphicsGetCurrentContext()];
-        UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-        UIGraphicsEndImageContext();
-        return [[UIImageView alloc] initWithImage:image];
-    }
+    // Snapshotting a view breaks on an iPhone 6 if you don't have the right launch image...
+    //    if ([self respondsToSelector:@selector(snapshotViewAfterScreenUpdates:)]) {
+    //        return [self snapshotViewAfterScreenUpdates:NO];
+    //    } else {
+    UIGraphicsBeginImageContextWithOptions(self.bounds.size, self.isOpaque, 0.0f);
+    [self.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return [[UIImageView alloc] initWithImage:image];
+    //    }
 }
 
 @end
